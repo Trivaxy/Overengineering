9f2ec8568ee4b4caf242e139ab9f57edb9c3bd66
OSGAGaming <tafida.a123@gmail.com>
2021-12-06 19:56:03 +0000

    Renderer doesn't work :P

--- Program.cs
+++ Program.cs
@@ -5,13 +5,31 @@ namespace Overengineering
 {
 	public class Program : Game
 	{
+		public static Renderer Renderer { get; set; }
+		public static Game Instance;
+
 		static void Main(string[] args)
 		{
 			using (Program program = new Program())
 			{
-				new GraphicsDeviceManager(program);
 				program.Run();
 			}
+		}
+
+		public Program()
+		{
+			Renderer = new Renderer(this);
+			Window.AllowUserResizing = true;
+			Content.RootDirectory = "Content";
+			IsMouseVisible = true;
+			IsFixedTimeStep = true;
+		}
+
+        protected override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+
+			Renderer.Draw();
 		}
 	}
 }
---
+++ Renderer.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace Overengineering
+{
+    public class Renderer
+    {
+        public virtual Point MaxResolution { get; set; } = new Point(2560, 1440);
+
+        public GraphicsDeviceManager Graphics { get; set; }
+        public RenderTarget2D RenderTarget { get; set; }
+        public Game Instance { get; set; }
+        public SpriteBatch Spritebatch { get; set; }
+
+
+        public Rectangle RenderDestination;
+
+        public Renderer(Game game)
+        {
+            Instance = game;
+
+            Graphics = new GraphicsDeviceManager(Instance)
+            {
+                GraphicsProfile = GraphicsProfile.HiDef
+            };
+            Graphics.SynchronizeWithVerticalRetrace = true;
+            Graphics.ApplyChanges();
+
+            int X = MaxResolution.X;
+            int Y = MaxResolution.Y;
+
+            RenderTarget = new RenderTarget2D(Graphics?.GraphicsDevice, X, Y);
+            Spritebatch = new SpriteBatch(Graphics.GraphicsDevice);
+        }
+
+        public void Draw()
+        {
+            Spritebatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend);
+
+            Spritebatch.Draw(RenderTarget, RenderDestination, Color.White);
+
+            Spritebatch.End();
+        }
+    }
+}
